21d9864b57ed6395695f7aa0c3db196381b3e258
yutong pang <yutongpang@yutongs-iMac.home>
2015-07-16 08:48:02 -0400

    regularization with geneticalgorithm

--- RegularizationCore/Regularization.h
+++ RegularizationCore/Regularization.h
@@ -4,5 +4,49 @@
 
 #ifndef REGULARIZATION_REGULARIZATION_H
 #define REGULARIZATION_REGULARIZATION_H
+#include <vector>
+#define GA_POPSIZE 20000
+#define GA_MAXITER 10000
+#define GA_ELITRATE 0.1f
+#define GA_MUTATION	RAND_MAX * GA_MUTATIONRATE
+#define GA_MUTATIONRATE 0.7f
+#define FSIZE 20
+#define GA_TARGET_SIZE 2
 
+using namespace std;
+typedef vector<double > imp_vector;
+typedef vector<vector<double>> operator_matrix;
+typedef vector<double> measurement;
+struct ga_struct
+{
+    imp_vector freconstruct;
+    double fitness;
+    ga_struct(){
+        freconstruct.assign(FSIZE, 0.0);
+    }
+};
+
+typedef vector<ga_struct> ga_vector;// for brevity
+
+class GeneticAlgorithm{
+public:
+    GeneticAlgorithm();
+    void runenvolotion();
+
+private:
+    operator_matrix A;
+    double d {0.25};
+    measurement b;
+    vector<double> fexact;
+    void init_population(ga_vector &population,
+                         ga_vector &buffer );
+    void calc_fitness(ga_vector &population);
+    inline void print_best(ga_vector &gav);
+    void mate(ga_vector &population, ga_vector &buffer);
+    void elitism(ga_vector &population,
+                 ga_vector &buffer, int esize );
+    void mutate(ga_struct &member);
+    double fRand(double fMin, double fMax);
+
+};
 #endif //REGULARIZATION_REGULARIZATION_H
--- main.cpp
+++ main.cpp
@@ -1,2 +1,6 @@
 #include <iostream>
-using namespace std;int main(){cout<<"Hello, World!"<<endl;return 0;}
+#include "RegularizationCore/Regularization.h"
+using namespace std;int main(){
+    GeneticAlgorithm geneticAlgorithm;
+    geneticAlgorithm.runenvolotion();
+}
